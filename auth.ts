3a8874c59a0b0d6f53051506aa6850fbcb252da2
GiFTED <gkenny896@gmail.com>
2024-06-10 13:36:42 +0100

    feat

--- auth.ts
+++ auth.ts
@@ -1,26 +1,8 @@
 import NextAuth from "next-auth";
 import "next-auth/jwt";
 
-import Apple from "next-auth/providers/apple";
-import Cognito from "next-auth/providers/cognito";
-import Coinbase from "next-auth/providers/coinbase";
-import Discord from "next-auth/providers/discord";
-import Dropbox from "next-auth/providers/dropbox";
 import GitHub from "next-auth/providers/github";
-import GitLab from "next-auth/providers/gitlab";
-import Hubspot from "next-auth/providers/hubspot";
-import Keycloak from "next-auth/providers/keycloak";
-import LinkedIn from "next-auth/providers/linkedin";
-import Netlify from "next-auth/providers/netlify";
-import Okta from "next-auth/providers/okta";
-import Passage from "next-auth/providers/passage";
-import Passkey from "next-auth/providers/passkey";
-import Pinterest from "next-auth/providers/pinterest";
-import Reddit from "next-auth/providers/reddit";
-import Slack from "next-auth/providers/slack";
-import Spotify from "next-auth/providers/spotify";
-import Twitch from "next-auth/providers/twitch";
-import Zoom from "next-auth/providers/zoom";
+
 import { createStorage } from "unstorage";
 import memoryDriver from "unstorage/drivers/memory";
 import vercelKVDriver from "unstorage/drivers/vercel-kv";
@@ -40,36 +22,7 @@ const storage = createStorage({
 const config = {
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   adapter: UnstorageAdapter(storage),
-  providers: [
-    Apple,
-    Cognito,
-    Coinbase,
-    Discord,
-    Dropbox,
-    GitHub,
-    GitLab,
-    Hubspot,
-    Keycloak({ name: "Keycloak (bob/bob)" }),
-    LinkedIn,
-    Netlify,
-    Okta,
-    Passkey({
-      formFields: {
-        email: {
-          label: "Username",
-          required: true,
-          autocomplete: "username webauthn",
-        },
-      },
-    }),
-    Passage,
-    Pinterest,
-    Reddit,
-    Slack,
-    Spotify,
-    Twitch,
-    Zoom,
-  ],
+  providers: [GitHub],
   basePath: "/auth",
   callbacks: {
     authorized({ request, auth }) {
